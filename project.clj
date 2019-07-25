947f4c1c4322eb0247b7ae75f37efe52951bdf9d
filip <fbielejec@gmail.com>
2019-07-25 21:47:54 +0200

    typo

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
 
-  :dependencies [[district0x.re-frame/ipfs-fx "1.1.1"]
+  :dependencies [[district0x/re-frame-ipfs-fx "1.1.1"]
                  [mount "0.1.11"]
                  [org.clojure/clojurescript "1.10.439"]
                  [re-frame "0.10.2"]]
