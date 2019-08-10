3393f80cd9394b8b97b708fcc1a8aed110efd1d3
Filip Bielejec <fbielejec@gmail.com>
2019-08-10 11:42:59 +0100

    deps++ (#2)
    * deps++

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject district0x/district-ui-ipfs "1.0.0-SNAPSHOT"
+(defproject district0x/district-ui-ipfs "1.0.1-SNAPSHOT"
   :description "district UI module for setting up IPFS"
   :url "https://github.com/district0x/district-ui-ipfs"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
 
-  :dependencies [[district0x.re-frame/ipfs-fx "1.0.0"]
+  :dependencies [[district0x/re-frame-ipfs-fx "1.1.1"]
                  [mount "0.1.11"]
                  [org.clojure/clojurescript "1.10.439"]
                  [re-frame "0.10.2"]]
@@ -23,6 +23,19 @@
                    :plugins [[lein-cljsbuild "1.1.7"]
                              [lein-doo "0.1.8"]
                              [lein-npm "0.6.2"]]}}
+
+  :deploy-repositories [["snapshots" {:url "https://clojars.org/repo"
+                                      :username :env/clojars_username
+                                      :password :env/clojars_password
+                                      :sign-releases false}]
+                        ["releases"  {:url "https://clojars.org/repo"
+                                      :username :env/clojars_username
+                                      :password :env/clojars_password
+                                      :sign-releases false}]]
+
+  :release-tasks [["vcs" "assert-committed"]
+                  ["change" "version" "leiningen.release/bump-version" "release"]
+                  ["deploy"]]
 
   :cljsbuild {:builds [{:id "tests"
                         :source-paths ["src" "test"]
